b88e88d3bb8da366e270fcf8c541108e9e83782c
Key & Codes <andrewgalvez@outlook.com>
2023-01-08 10:41:59 -0500

    added comments to fizzbuzz.cpp

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -1,3 +1,8 @@
+//A program that outputs numbers from 1 to 100. 
+//But for multiples of 3, print Fizz instead of the number
+//For the multiples of 5, print Buzz
+//For numbers which are multiples of both 3 and 5 print, FizzBuzz
+
 #include <iostream>
 
 int main () {
